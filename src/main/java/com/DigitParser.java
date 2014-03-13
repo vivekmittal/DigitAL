99e1a9a3747d1e1c9d86e4cbdc1d83aae033e484
Vivek Mittal <vmittal@aconex.com>
2014-03-13 19:29:59 +0530

    Modified Tests to have more readability

--- src/main/java/com/DigitParser.java
+++ src/main/java/com/DigitParser.java
@@ -15,10 +15,12 @@ public class DigitParser {
     }
 
     public List<Digit> parse() {
-        List<Digit> digits= newArrayList();
+        List<Digit> digits = newArrayList();
 
         for (int i = 0; i < NUMBER_OF_DIGITS_IN_ACCOUNT_NUMBER; i++) {
-            digits.add(digitAt(i));
+            digits.add(
+                digitAt(i)
+            );
         }
 
         return digits;
@@ -29,7 +31,7 @@ public class DigitParser {
 
         return Digit.digitFor(
                 strings.get(position) +
-                strings.get(position + NUMBER_OF_DIGITS_IN_ACCOUNT_NUMBER) +
-                strings.get(position + NUMBER_OF_DIGITS_IN_ACCOUNT_NUMBER * 2));
+                        strings.get(position + NUMBER_OF_DIGITS_IN_ACCOUNT_NUMBER) +
+                        strings.get(position + NUMBER_OF_DIGITS_IN_ACCOUNT_NUMBER * 2));
     }
 }
--- src/test/java/com/DigitReaderTest.java
+++ src/test/java/com/DigitReaderTest.java
@@ -1,5 +1,6 @@
 package com;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayInputStream;
@@ -11,12 +12,21 @@ import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
 @Test
 public class DigitReaderTest {
-    public void shouldReadDigitFromStream() throws Exception {
+
+    private DigitsReader digitsReader;
+
+    @BeforeMethod
+    public void setUp() {
+        digitsReader = new DigitsReader();
+    }
+
+    public void readDigitsFromRawText() throws Exception {
         String line1 = " _  _     _  _  _  _  _  _ \r\n"
                      + " _| _||_||_ |_   ||_||_|| |\r\n"
-                     + "|_  _|  | _||_|  ||_| _||_|\r\n";
+                     + "|_  _|  | _||_|  ||_| _||_|\r\n"
+                     + "                               ";
 
-        List<Digit> digits = new DigitsReader().read(new ByteArrayInputStream(line1.getBytes()));
+        List<Digit> digits = digitsReader.read(new ByteArrayInputStream(line1.getBytes()));
 
         assertThat(digits, contains(TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, ZERO));
     }
